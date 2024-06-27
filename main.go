46a4863fe5f5b953c376b5c8904d17f12eebe50b
Sarthak K <37489648+potatoe-hooman@users.noreply.github.com>
2024-06-27 14:12:12 +0000

    committing from repl.it

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
-go
+  package main
+
+  import (
+    "fmt"
+  )
+
+  func main() {
+    const name, age = "Kim", 22
+    fmt.Println(name, "is", age, "years old.")
+
+    // It is conventional not to worry about any
+    // error returned by Println.
+
+  }
